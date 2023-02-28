b1444b3f7c06e0c27c4c8cb9608f1ca58693ad6b
Anna-Carla <annacarlateixeira16@gmail.com>
2023-02-28 20:57:34 -0300

    add atv

--- Lista 2/atv4.java
+++ Lista 2/atv4.java
@@ -1,6 +1,25 @@
 import java.util.*;
 public class atv4 {
     public static void main(String[] args) {
-    
+    Scanner sc = new Scanner (System.in);
+    int x, y;
+
+    System.out.print("Digite o x: ");
+    x = sc.nextInt();
+    System.out.print("Digite o y: ");
+    y = sc.nextInt();
+
+    System.out.println("Resultado: " + mdc(x, y));
+    sc.close();
+    }
+
+    public static int mdc (int x, int y){
+        if(x==y){
+            return x;
+        }
+        if(x>y){
+            return mdc(x-y, y);
+        }
+            return mdc(x, y-x);
     }
 }
